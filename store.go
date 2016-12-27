ccf256eeca356861d6e261cc974f8d73b92882df
Andrii Zadorozhnii <doxtop@synrc.com>
2016-12-27 15:13:29 +0200

    store named counter

--- store.go
+++ store.go
@@ -6,48 +6,48 @@ import (
   "strconv"
   "golang.org/x/net/context"
   "google.golang.org/appengine/datastore"
-  "google.golang.org/appengine/log"
+  //"google.golang.org/appengine/log"
 )
 
 const(
   countGroup = 25
 )
 
+func (e *Entity) Key(key *datastore.Key) {e.Name = key.Encode()}
 func (e *Entity) Set(v string){e.Payload = v}
-
 func (e *Entity) Point() {e.Payload = "0"}
-
-func (e *Entity) Inc(e1 *Entity) (err error) { 
-  i1,err := strconv.ParseUint( e.Payload, 10, 64)
-  i2,err := strconv.ParseUint(e1.Payload, 10, 64)
-  e.Payload = fmt.Sprintf("%d", i1+i2)
-  return
-}
 
 /*
  * Store. check negative update effect
  */
 func (e *Entity) Store(ctx context.Context, kind string) (key *datastore.Key, err error) {
-  log.Infof(ctx, "Store %v %v", kind, e)
-
-  name:= fmt.Sprintf("%d",rand.Intn(countGroup))
+  total,err := strconv.ParseUint(e.Payload, 10, 64)
+  if(err!=nil){
+    return
+  }
+  name:= fmt.Sprintf("%s-%d", e.Name, rand.Intn(countGroup))
   key = datastore.NewKey(ctx,kind,name,0,nil)
-
+  
   err = datastore.RunInTransaction(ctx, func(ctx context.Context) error {
-    e1:=new(Entity)
-    e1.Point() //ErrNoSuchEntity
+    e1 := Entity{"", "0"}
 
-    if err := datastore.Get(ctx, key, e1); err!=nil && err != datastore.ErrNoSuchEntity{
+    if err := datastore.Get(ctx, key, &e1); err!=nil && err != datastore.ErrNoSuchEntity{
       return err
     }
-    
-    if err := e.Inc(e1); err!=nil { return err }
 
+    v,err := strconv.ParseUint(e1.Payload, 10, 64)
+    if (err != nil){
+      return err
+    }
+    total += v
+
+    e.Set(strconv.FormatUint(uint64(total), 10))
+    
     _,err = datastore.Put(ctx,key,e);
 
     return err
 
-  }, nil/* once */)
+  }, nil)
   return
 }
 
@@ -55,19 +55,23 @@ func (e *Entity) Store(ctx context.Context, kind string) (key *datastore.Key, er
  * Fold 
  */
 func (e *Entity) Count(ctx context.Context, kind string) (err error) {
-  e.Point()
-  q := datastore.NewQuery("Counter")//.Limit(countGroup)
+  q := datastore.NewQuery("Counter").Filter("name=", e.Name)
   t := q.Run(ctx)
-
+  
+  var total uint64 = 0
   for {
-    e1 :=new(Entity)
-    _,err := t.Next(e1)
+    e1 := Entity{"", "0"}
+    _,err := t.Next(&e1)
+
     if err == datastore.Done { break }
-    if err!=nil { 
-      log.Infof(ctx,"Count error %s", err.Error())
-      return err
+    if err!=nil { return err }
+    
+    v,err := strconv.ParseUint(e1.Payload, 10, 64); 
+    if err!=nil {
+      break
     }
-    err = e.Inc(e1)
+    total += v
   }
+  e.Set(strconv.FormatUint(uint64(total), 10))
   return
 }
